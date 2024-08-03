8016f6aacba9c674f4148f65aa91aa4a9c5a3f36
Jesu <jesusdiaz013@gmail.com>
2024-08-03 13:04:09 -0300

    build-your-pc-styles-improved

--- src/app/core/services/build_your_pc/build-your-pc.service.ts
+++ src/app/core/services/build_your_pc/build-your-pc.service.ts
@@ -4,33 +4,101 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class BuildYourPcService {
-  
+
   constructor() { }
 
+  buildYourPcCart: BuildYourPcCart = {
+    Procesador: 'Procesador gamer Intel Core i9-13900K (9a. generación)',
+    Motherboard: 'Motherboard Asus Prime A320m-k Am4 Ddr4 ATX',
+    MemoriaRAM: 'Hyper X ddr4 3200 mhz 16 Gb',
+    Almacenamiento: 'Disco sólido interno Crucial CT240BX500SSD1 240GB negro',
+    FuenteDePoder: 'XCORE Power XCP630-TS 3200 mhz 16 Gb 80+ gold',
+    TarjetaDeVideo: null,
+    SistemaDeEnfriamiento: null,
+    Monitor: null,
+    Audifonos: null,
+    Teclado: null,
+    Mouse: null,
+  };
 
-  
+  getBuildYourPcCart() {
+    return this.buildYourPcCart;
+  }
 }
 
 interface BuildYourPcCart {
-//   Processor:string;
-
-// Motherboard:string;
-
-// Memory RAM:string;
-
-// Storage device:string;
-
-// Power suply:string;
-
-// Video card:string;
-
-// Cooling system:string;
-
-// Monitor:string;
-
-// Headset:string;
-
-// Keyboard:string;
-
-// Mouse:string;
+  Procesador: string | null;
+  Motherboard: string | null;
+  MemoriaRAM: string | null;
+  Almacenamiento: string | null;
+  FuenteDePoder: string | null;
+  TarjetaDeVideo: string | null;
+  SistemaDeEnfriamiento: string | null;
+  Monitor: string | null;
+  Audifonos: string | null;
+  Teclado: string | null;
+  Mouse: string | null;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//   constructor() { }
+
+//   buildYourPcCart: BuildYourPcCart = {
+//     processor: 'Procesador gamer Intel Core i9-13900K (9a. generación)',
+//     motherboard: 'Motherboard Asus Prime A320m-k Am4 Ddr4 ATX ',
+//     memory_ram: ' Hyper X ddr4 3200 mhz 16 Gb',
+//     storage_device: 'Disco sólido interno Crucial CT240BX500SSD1 240GB negro',
+//     power_supply: 'XCORE Power XCP630-TS  3200 mhz 16 Gb 80+ gold',
+//     video_card: null,
+//     cooling_system: null,
+//     monitor:null,
+//     headset:  null,
+//     keyboard: null,
+//     mouse: null,
+//   };
+
+//   getBuildYourPcCart() {
+//     return this.buildYourPcCart;
+//   }
+
+// }
+
+// interface BuildYourPcCart {
+//   processor: string | null;
+//   motherboard: string | null;
+//   memory_ram: string | null;
+//   storage_device: string | null;
+//   power_supply: string | null;
+//   video_card: string | null;
+//   cooling_system: string | null;
+//   monitor: string | null;
+//   headset: string | null;
+//   keyboard: string | null;
+//   mouse: string | null;
+// }
--- src/app/pages/store/build-your-pc/steps-of-the-paths/steps-of-the-paths.component.ts
+++ src/app/pages/store/build-your-pc/steps-of-the-paths/steps-of-the-paths.component.ts
@@ -1,11 +1,13 @@
-import { Component, EventEmitter, Input, Output, output } from '@angular/core';
+import { NgClass, NgFor } from '@angular/common';
+import { Component, EventEmitter, inject, Input, Output, output } from '@angular/core';
 import { MatIcon } from '@angular/material/icon';
 import { OutletContext } from '@angular/router';
+import { BuildYourPcService } from 'app/core/services/build_your_pc/build-your-pc.service';
 
 @Component({
   selector: 'app-steps-of-the-paths',
   standalone: true,
-  imports: [MatIcon],
+  imports: [MatIcon,NgFor,NgClass],
   templateUrl: './steps-of-the-paths.component.html',
   styleUrl: './steps-of-the-paths.component.scss'
 })
@@ -13,6 +15,18 @@ export class StepsOfThePathsComponent {
 
   @Input() show : boolean = true;
   @Output() close = new EventEmitter<void>();
+
+  _buildYourPcCartService = inject(BuildYourPcService);
+  cart$ !: any;
+  ngOnInit(): void {
+    this.cart$ = this._buildYourPcCartService.getBuildYourPcCart();
+  }
+
+  getCartEntries() {
+    console.log(Object.entries(this.cart$));
+    return Object.entries(this.cart$ );
+  }
+
 
   closeMenu(){
     this.show=false;
